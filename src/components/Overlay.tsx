c7a75603a8a0b2b638702372e1ed0fe0164a3fbc
GRUSPAT <patrykgruszowski@gmail.com>
2024-06-09 16:28:51 +0200

    add: bottom bar

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -2,8 +2,15 @@ import '../styles/overlay.css'
 
 export const Overlay = () => {
     return(
+        <>
         <nav className="overlay">
             <p>PATRYK <a className="portfolio"href="">PORTFOLIO&nbsp;</a><a className="about" href="">ABOUT</a><br></br>GRUSZOWSKI&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a className="contact" href="">CONTACT</a></p>
         </nav>
+        <div className="marquee-container">
+        <div className="marquee">
+            <span>*This site is currently under development and its core feature aren't available yet. Please consider visiting this site in future.*</span>
+        </div>
+    </div>
+        </>
     )
 }
