529cc12d0660bc52b6c182273350beba8862700f
GRUSPAT <patrykgruszowski@gmail.com>
2024-07-01 22:04:40 +0200

    fix: minor changes

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -4,7 +4,13 @@ export const Overlay = () => {
     return(
         <>
         <nav className="overlay">
-            <p>PATRYK <a className="portfolio"href="">PORTFOLIO&nbsp;</a><a className="about" href="">ABOUT</a><br></br>GRUSZOWSKI&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a className="contact" href="">CONTACT</a></p>
+            <p>PATRYK 
+                <a className="portfolio"href="">PORTFOLIO&nbsp;</a>
+                <a className="about" href="">ABOUT</a>
+                <br></br>GRUSZOWSKI
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a className="contact" href="">CONTACT</a>
+            </p>
         </nav>
         <div className="marquee-container">
         <div className="marquee">
