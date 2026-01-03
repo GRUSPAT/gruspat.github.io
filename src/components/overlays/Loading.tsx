4352671f0240e4b3cb013b97cf4581c669665599
GRUSPAT <patrykgruszowski@gmail.com>
2026-01-03 21:04:11 +0100

    Added gallery

--- src/components/overlays/Loading.tsx
+++ src/components/overlays/Loading.tsx
@@ -1,5 +1,6 @@
 import '../../styles/loading.css'
-import CurvedLoop from './CurvedLoop';
+//import CurvedLoop from './CurvedLoop';
+import DomeGallery from './DomeGallery';
 import Aurora from './Aurora';
 
 export const Loading = () => {
@@ -10,13 +11,16 @@ export const Loading = () => {
   colorStops={["#5227FF", "#FF6B00", "#fff"]} 
   amplitude={1.2}
 />
-            
-            <CurvedLoop
-             interactive={false}
-             marqueeText='powstaje · nowa · strona ·'
-             speed={1}
-             
-            />
+            <div style={{ width: '100vw', height: '100vh'}}>
+      <DomeGallery fit={0.5}
+        minRadius={1000}
+        segments={38}
+        padFactor={0}
+        fitBasis={'width'}
+        maxVerticalRotationDeg={3}
+        grayscale={false}/>
+    </div>
+           
       
         </>
     )
